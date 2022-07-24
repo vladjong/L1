482ee9d4aae18c7045119ee684757593a1fe897d
adough <adough@student.21-school.ru>
2022-07-24 22:52:58 +0700

    add ex5 and ex6(2 method)

--- ex5/ex5.go
+++ ex5/ex5.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+/*
+	Способ №1 с использованием time.After.
+	After ожидает истечение продолжительности времени,
+	а затем отправляет текущее время в канал
+*/
 func main() {
 	fmt.Println("Enter time duration of work:")
 	choseTime := 0
@@ -14,24 +19,32 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go read(ch, &wg)
-	write(ch, choseTime, &wg)
+	go write(ch, choseTime)
+	wg.Wait()
 }
 
+/*
+	Функция чтение записи из канала,
+	перебираем все значения полученные из канала
+*/
 func read(ch chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
 	for item := range ch {
 		fmt.Println(item)
 	}
+	defer wg.Done()
 }
 
-func write(ch chan string, choseTime int, wg *sync.WaitGroup) {
+/*
+	Функция записи в канал, если время истекает,
+	то мы переходмс в кейс timeout, закрываем канал.
+*/
+func write(ch chan string, choseTime int) {
 	timeout := time.After(time.Duration(choseTime) * time.Second)
 	for {
 		select {
 		case <-timeout:
 			fmt.Println("My time is up\nGoodbye!")
 			close(ch)
-			wg.Wait()
 			return
 		default:
 			ch <- "Hello"
@@ -39,3 +52,39 @@ func write(ch chan string, choseTime int, wg *sync.WaitGroup) {
 		}
 	}
 }
+
+/*
+	Способ №2 с использованием context.WithTimeout.
+	After ожидает истечение продолжительности времени,
+	а затем отправляет текущее время в канал
+*/
+
+/*
+func main() {
+	fmt.Println("Enter time duration of work:")
+	choseTime := 0
+	fmt.Scan(&choseTime)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	wg := new(sync.WaitGroup)
+	ch := make(chan string)
+	wg.Add(1)
+	go write(ctx, ch)
+	go reader(ch, wg)
+	wg.Wait()
+}
+
+func write(ctx context.Context, ch chan<- string) {
+	for {
+		select {
+		default:
+			ch <- "Hello"
+			time.Sleep(time.Millisecond * 100)
+		case <-ctx.Done():
+			fmt.Println("My time is up\nGoodbye!")
+			close(ch)
+			return
+		}
+	}
+}
+*/
--- ex6/ex6.go
+++ ex6/ex6.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	/*
+		Способ №1 WaitGroup
+	*/
+	fmt.Println("WaitGroup start")
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go waitGroupeStop(&wg)
+	wg.Wait()
+	fmt.Println("WaitGroup stop")
+	/*
+		Способ №2 Context
+	*/
+	fmt.Println("Context start")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg1 := sync.WaitGroup{}
+	wg1.Add(1)
+	go contextStop(ctx)
+	time.Sleep(time.Microsecond * 30)
+	cancel()
+	fmt.Println("Context stop")
+
+}
+
+func waitGroupeStop(wg *sync.WaitGroup) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	defer wg.Done()
+}
+
+func contextStop(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fmt.Println("Doing something")
+		}
+	}
 }
